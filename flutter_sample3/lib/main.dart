e4d5c937d23011864c0efbb93508e0cfae535a67
DanishKhan <danishkhan.jamia@gmail.com>
2019-05-19 07:42:02 +0530

    Project from 0 level.
    This is very first samples of project which will help you learn Flutter as beginner

---
+++ flutter_sample3/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class  HelloRectangle extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.orange,
+        height: 400.0,
+        width: 300.0,
+        child: Center(
+          child: Text(
+            "Hello",
+            style: TextStyle(fontSize: 40),),
+        ),
+      ),
+    );
+  }
+
+}
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello Rectangle"),
+        ),
+        body: HelloRectangle(),
+        ),
+      ),
+    );
+}
